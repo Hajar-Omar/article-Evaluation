bc0ecd64a184a324d7f3b5d51ae266203b660741
Hajar <hajaro@banquemisr.com>
2021-05-18 22:27:28 +0200

    stable version, main functionality

--- src/client/js/base.js
+++ src/client/js/base.js
@@ -1,9 +1,35 @@
-export function handleSubmit(event) {
-  event.preventDefault();
-  console.log(event);
-  document.getElementById("result").innerHTML = "hello";
+const textAreaEle = document.getElementById("txt");
+const resultElm = document.getElementById("result");
+
+function checkUrl(url) {
+  return new RegExp(
+    /https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,}/
+  ).test(url);
 }
 
-document.getElementById("btn").addEventListener("click", (e) => {
+export function handleSubmit(event) {
+  event.preventDefault();
+  resultElm.innerHTML = "";
+  // if url is valid, call the api, else show wrong feedback
+  if (checkUrl(textAreaEle.value)) {
+    fetch(`http://localhost:3000/api/evaluate?url=${textAreaEle.value}`)
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.agreement)
+          resultElm.innerHTML = `<ol><li>Agreement: ${res.agreement}</li><li>irony: ${res.irony}</li><li>model: ${res.model}</li><li>confidence: ${res.confidence}</li></ol>`;
+        else resultElm.innerHTML = "<span class='error'>An Invalid URL</span>";
+      })
+      .catch(
+        (err) =>
+          (resultElm.innerHTML = "<span class='error'>An Invalid URL</span>")
+      );
+  } else resultElm.innerHTML = "<span class='error'>An Invalid URL</span>";
+}
+
+document.getElementById("submit-btn").addEventListener("click", (e) => {
+  handleSubmit(e);
+});
+
+textAreaEle.addEventListener("blur", (e) => {
   handleSubmit(e);
 });
--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,4 @@
-var FormData = require("form-data");
-const request = require('request');
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,90 +12,20 @@ app.use(cors());
 const dotenv = require("dotenv");
 dotenv.config();
 
-console.log(`Your API key is ${process.env.API_KEY}`);
-
-// const textapi = new MeaningCloud({
-//   application_key: process.env.API_KEY,
-// });
-
 app.use(express.static("dist"));
 
 app.get("/", function (req, res) {
   res.sendFile("dist/index.html");
-})
+});
 
-app.get('/api/test', function(req,res){
-
-    const formdata = new FormData();
-    formdata.append("key", process.env.API_KEY);
-    formdata.append("txt", "Hello I'm so happy");
-    formdata.append("lang", "en"); // 2-letter code, like en es fr ...
-
-    const requestOptions = {
-        method: "POST",
-        body: formdata,
-        redirect: "follow",
-      };
-
-      request({
-        method:'POST',
-        body:formdata,
-        redirect: "follow",
-        url:"https://api.meaningcloud.com/sentiment-2.1"
-      }, function (err, postRes, body) {
-          console.log( body)
-          res.send(postRes.toJSON())
-        // if (err) {
-        //   res.writeHead(500, { 'Content-Type': 'application/json' });
-        //   var obj = {};
-        //   obj['Status'] = 'something went wrong: ' + err;
-        //   res.end( JSON.stringify( obj ) );
-        // }
-        // else {
-        //     // res.send(`inside Book is added to the database${req.body}`);
-        //   res.writeHead(200, { 'Content-Type': 'application/json' });
-        //   var obj = {};
-        //   obj['Status'] = postRes.statusCode;
-        //   res.end( JSON.stringify( obj ) );
-        // }
-      })
-    // res.send(`Book is added to the database ${req}`);
-
-    // app.post(
-    //     "https://api.meaningcloud.com/sentiment-2.1",
-    //     requestOptions,
-    //     function (req, res) {
-    //       console.log(req,res);
-    //     }
-    //   );
-})
-
-
-
-
-app.get('/api/fetch', async function(req,res){
-    const formdata = new FormData();
-    formdata.append("key", process.env.API_KEY);
-    formdata.append("txt", "Main dishes were quite good, but desserts were too sweet for me");
-    formdata.append("lang", "en"); // 2-letter code, like en es fr ...
-
-const requestOptions = {
-  method: 'POST',
-  body: formdata,
-  redirect: 'follow'
-};
-
-    const response = await fetch("https://api.meaningcloud.com/sentiment-2.1", requestOptions)
-  .then(response =>  response)
-//   .then(response => {
-//     //   console.log( response);
-//     //   res.send( response)
-// })
-  .catch(error => console.log('error', error));
-  console.log(response)
-  res.send(response)
-})
-
+app.get("/api/evaluate", function (req, res) {
+  fetch(
+    `https://api.meaningcloud.com/sentiment-2.1?key=${process.env.API_KEY}&url=${req.query.url}&lang=en`
+  )
+    .then((response) => response.json())
+    .then((response) => res.send(response))
+    .catch((error) => res.send(error));
+});
 
 // designates what port the app will listen to for incoming requests
 app.listen(port, function () {
